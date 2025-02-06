4e7756e90f7caa763fde06b48dfabee77f90482f
Josh Monahan <g00409871@atu.ie>
2025-02-06 16:42:32 +0000

    add GET endpoint to retrieve automations and format response data

--- backend/routes/automation_routes.py
+++ backend/routes/automation_routes.py
@@ -3,3 +3,16 @@ from bson import ObjectId
 from db import automations_collection, devices_collection
 
 automation_routes = Blueprint('automations', __name__)
+
+@automation_routes.route('/', methods=['GET'])
+def get_automations():
+    try:
+        automations = list(automations_collection.find())
+        for automation in automations:
+            automation['id'] = str(automation['_id'])
+            if 'deviceId' in automation:
+                automation['deviceId'] = str(automation['deviceId'])
+            del automation['_id']
+        return jsonify(automations)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
