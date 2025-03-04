e3974678d8f2c112aa885880d6751817fad5bae1
davidoc23 <g00400530@atu.ie>
2025-03-04 13:01:10 +0000

    refactor: remove unused Device interface and related functions, along with configuration files

--- smart-home-hub/src/devices/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Device {
-    id: number;
-    name: string;
-    status: string;
-}
-
-export function manageDevice(device: Device) {
-    console.log(`Managing device: ${device.name}`);
-}
